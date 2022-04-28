893af86830fea2b3e3391de9176c1747a966a934
TR Kizaki <trkizaki.web@gmail.com>
2022-04-28 20:00:02 +0400

    add dependencies

--- model/repository/database.go
+++ model/repository/database.go
@@ -10,7 +10,7 @@ var Db *sql.DB
 func init() {
 	var err error
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"winelist-app", "winelist-password", "sample-api-db:3306", "winelist",
+		"winelist-app", "winelist-password", "gewineapi-db:3306", "winelist",
 	)//DB/docker-compose.yml and value of Dockerfile 
 	Db, err = sql.Open("mysql", dataSourceName)//Call sql.Open to initialize the db variable, passing the return value of dataSourceName.
 	if err != nil {
